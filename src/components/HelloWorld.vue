8bf388bcbe47704e04f6feb7f0a8b58440b34a2b
周玉恒 <Zyh@sqzx163>
2024-06-18 11:23:06 +0800

    6/18

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,42 @@
 <template>
-  <div class="hello" style="margin: 0 auto;">
-    <el-button @click="sign('sign')">签名</el-button>
-    <el-button @click="sign('seal')">盖章</el-button>
-    <Signature-vue ref="SignatureForm"/>
-  </div>
+    <div class="hello" style="margin: 0 auto">
+        <el-button @click="sign('sign')">签名</el-button>
+        <el-button @click="sign('seal')">盖章</el-button>
+        <el-button @click="sign('create')">制作印章</el-button>
+        <Signature-vue ref="SignatureForm" />
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  methods:{
-    sign(type){
-      this.show = true
-      this.$nextTick(() => {
-        this.$refs.SignatureForm.init(type)
-      })
-    }
-  }
-}
+    name: "HelloWorld",
+    props: {
+        msg: String,
+    },
+    methods: {
+        sign(type) {
+            if (type == "create") {
+                window.open(
+                    "http://10.7.22.102:8000/",
+                    "_blank",
+                    "toolbar=yes,width=1200,height=800"
+                );
+            } else {
+                this.show = true;
+                this.$nextTick(() => {
+                    this.$refs.SignatureForm.init(type);
+                });
+            }
+        },
+    },
+};
 </script>
 <style>
-.hello{
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.hello {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
